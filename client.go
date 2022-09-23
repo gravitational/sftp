144833d0f1c9330133d6e4dbd7d990684fec312d
Andrew LeFevre <andrew.lefevre@goteleport.com>
2022-09-23 10:24:49 -0400

    propagate EEXIST errors to client

--- client.go
+++ client.go
@@ -1916,6 +1916,8 @@ func normaliseError(err error) error {
 			return os.ErrNotExist
 		case sshFxPermissionDenied:
 			return os.ErrPermission
+		case sshFxFileAlreadyExists:
+			return os.ErrExist
 		case sshFxOk:
 			return nil
 		default:
